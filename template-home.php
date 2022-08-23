6d2bcf722d9d67e36e3cdc2a2129f3ecdd88babe
Nadia Akther <nadia98akther@gmail.com>
2022-08-23 20:44:43 +0600

    template of home page

---
+++ template-home.php
@@ -0,0 +1,593 @@
+<?php
+//Template Name: Home
+get_header();
+?>
+
+
+
+
+<!------plugin trusted------->
+<div class="container">
+    <div class="Frame_11">
+       <div class="Frame_15">
+         <p class="text1">Plugins Trusted by 1 Million+ WordPress Developers</p>
+       
+          <p class="text2">Come and Explore the best of WordPress and WooCommerce extensions that will help your websites and businesses thrive</p>
+       </div>
+       <!----buttton-->
+       
+       <div class="Frame_14">
+         <div class="Frame_12">
+            <div class="Visit_store">
+                <p>Visit Store</p>
+
+            </div>
+
+
+         </div>
+         <div class="Frame_13">
+            <div class="Login">
+                <p>Login</p>
+
+            </div>
+
+
+         </div>
+        
+       </div>
+    </div>
+
+
+
+
+
+</div>
+
+
+
+<!----------total reven image---------->
+
+<div class="Frame_16">
+ 
+ 
+  <div class="Group1">
+ 
+      <img class="group1_img" src="<?php echo get_template_directory_uri(); ?>/images/Group 1.png">
+      <img class="image2" src="<?php echo get_template_directory_uri(); ?>/images/image 2.png">
+ <img class="image1" src="<?php echo get_template_directory_uri(); ?>/images/image 1.png">
+   
+
+  </div>
+  
+ 
+
+
+</div>
+
+<!-----------Logo----------->
+
+<div class="Frame_18">
+    <p class="Text3">We Are Featured At</p>
+    <div class="Frame_19">
+        <div id="paypal-seeklogo.com1">
+            <img class="paypal1" src="<?php echo get_template_directory_uri(); ?>/images/paypal-seeklogo.com 1.png">
+
+        </div>
+        <div id="Frame_17">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Frame 17.png">
+        </div>
+        <div id="vector">
+            <img class="vector1" src="<?php echo get_template_directory_uri(); ?>/images/Vector.png">
+
+        </div>
+        <div id="paypal-seeklogo.com2">
+            <img class="paypal2" src="<?php echo get_template_directory_uri(); ?>/images/paypal-seeklogo.com 1.png">
+
+        </div>
+        <div id="Frame_18">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Frame 17.png">
+        </div>
+    </div>
+
+
+
+</div>
+
+
+
+<!-----------FEATURED PLUGIN---------->
+
+
+<div class="Frame_26">
+    
+    <div class="Frame_15_2nd">
+        <p class="Figma_15_text1">Our Featured Plugins</p>
+        <p class="Figma_15_text2">Designed for the best experience and developed to serve the most powerful eCommerce websites</p>
+
+    </div>
+    <div id="Figma_27">
+
+      <div id="#browser">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Browser.png">
+
+
+      </div>
+      <div class="Device">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/Device.png">
+
+      </div>
+
+    </div>
+    <div class="Frame_25">
+        <div class="Frame_22">
+         <div class="Frame_21">
+            <div id="bicalendar-check">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/Group.png">
+
+            </div>
+            <div class="Frame_19_2nd">
+                <p class="Frame_19_text1">WooCommerce Booking</p>
+                <p class="Frame_19_text2">Our booking add-on is absolutely simple and straightforward. It allows you to create a bookable product absolutely free.</p>
+
+            </div>
+            
+
+        
+         </div>
+         <div class="Frame_21">
+            <div id="bicalendar-check">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/Group.png">
+
+            </div>
+            <div class="Frame_19_2nd">
+                <p class="Frame_19_text1">WooCommerce Booking</p>
+                <p class="Frame_19_text2">Our booking add-on is absolutely simple and straightforward. It allows you to create a bookable product absolutely free.</p>
+
+            </div>
+            
+
+        
+         </div>
+         
+        </div>
+        <div class="Frame_22_2">
+            <div class="Frame_21">
+               <div id="bicalendar-check">
+                   <img src="<?php echo get_template_directory_uri(); ?>/images/Group.png">
+   
+               </div>
+               <div class="Frame_19_2nd">
+                   <p class="Frame_19_text1">WooCommerce Booking</p>
+                   <p class="Frame_19_text2">Our booking add-on is absolutely simple and straightforward. It allows you to create a bookable product absolutely free.</p>
+   
+               </div>
+               
+   
+           
+            </div>
+            <div class="Frame_21">
+               <div id="bicalendar-check">
+                   <img src="<?php echo get_template_directory_uri(); ?>/images/Group.png">
+   
+               </div>
+               <div class="Frame_19_2nd">
+                   <p class="Frame_19_text1">WooCommerce Booking</p>
+                   <p class="Frame_19_text2">Our booking add-on is absolutely simple and straightforward. It allows you to create a bookable product absolutely free.</p>
+   
+               </div>
+               
+   
+           
+            </div>
+           </div>
+           <div class="Frame_22_3">
+            <div class="Frame_21">
+               <div id="bicalendar-check">
+                   <img src="<?php echo get_template_directory_uri(); ?>/images/Group.png">
+   
+               </div>
+               <div class="Frame_19_2nd">
+                   <p class="Frame_19_text1">WooCommerce Booking</p>
+                   <p class="Frame_19_text2">Our booking add-on is absolutely simple and straightforward. It allows you to create a bookable product absolutely free.</p>
+   
+               </div>
+               
+   
+           
+            </div>
+            <div class="Frame_21">
+               <div id="bicalendar-check">
+                   <img src="<?php echo get_template_directory_uri(); ?>/images/Group.png">
+   
+               </div>
+               <div class="Frame_19_2nd">
+                   <p class="Frame_19_text1">WooCommerce Booking</p>
+                   <p class="Frame_19_text2">Our booking add-on is absolutely simple and straightforward. It allows you to create a bookable product absolutely free.</p>
+   
+               </div>
+               
+   
+           
+            </div>
+           </div>
+        
+        
+
+        </div>
+
+
+
+    </div>
+
+
+    <!-----------Customer SAY---------->
+
+<div class="Frame_36">
+    <div class="Frame_35_customer">
+        <p class="frame35_text1">What Our Customer Say</p>
+        <p class="frame35_text2">See what it's like to be a customer of SpringDevs</p>
+    </div>
+
+    <div class="Frame_37">
+        <div class="Frame_42">
+            <p class="Frame42_text1">I love the plugin UI and installed 4 different plugins within just 30 minutes. I was struggling to generate the PDF invoices and the support team resolved that within 10 minutes.</p>
+
+        <div class="Frame_41">
+            <p class="Frame41_text1">Nadia Akther</p>
+            <p class="Frame41_text2">Web Developer</p>
+
+        </div>
+
+        
+     
+
+       </div>
+       <div class="Frame_40_2">
+        <div id="biarrow-right">
+            
+                <i style='font-size:17px' class='fas'>&#xf060;</i>
+
+            
+
+        </div>
+
+
+       </div>
+       <div class="Frame_39_3">
+        <div id="biarrow-left">
+            
+            <i style='font-size:17px' class='fas'>&#xf060;</i>  
+
+        </div>
+
+
+       </div>
+    
+       
+
+
+    </div>
+    <div class="Frame_39">
+        <div class="Frame_38">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Frame 38.png">
+
+        </div>
+        <div class="Frame_39_2">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Frame 39.png">
+
+        </div>
+        <div class="Frame_40">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Frame 40.png">
+
+        </div>
+
+
+    </div>
+</div>
+
+<!----------Pricing------------------->
+
+
+    <div class="Frame_51">
+  <div class="Frame_15_3">
+   <p class="Frame15_2_text1">SpringDevs Pricing</p>
+  <p class="Frame15_2_text2">All of these license includes Free Plugin Update and Priority Support</p>
+   </div>
+   <div class="pricing_row">
+
+
+
+    <div id="Frame_28_2">
+     <div class="Frame_33">
+        <p class="Frame33_text1">1 Site</p>
+     <div class="Frame_31_1">
+        <div class="Frame_29_4">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame29_4_text1">1 Site license</p>
+        </div>
+        <div class="Frame_31_3">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame31_3_text1">Updates for 1 Year</p>
+        </div>
+        <div class="Frame_32_3">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame31_4_text1">Priority Email Support</p>
+        </div>
+        <div class="Frame_30_4">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame29_4_text1">1 Free Installation Service</p>
+        </div>
+        <div class="$299year">
+            <p><b class="price">$299</b> <b class="year">/Year</b></p>
+            <div class="Frame_12_2">
+             <p class="Frame12_2_button">Buy Now</p>
+
+            </div>
+            
+
+
+        </div>
+    </div>
+    </div>
+
+  
+
+  </div>
+
+
+  <div class="Frame_29_29">
+    <div class="Frame_33_33">
+       <div class="Frame_35_35">
+        <p class="Frame33_text1">5 Sites</p>
+        <div class="Frame_32_32">
+         <p class="Frame_32_32_t">Trending</p>
+        </div>
+        
+       </div>
+       <div class="Frame_31_1">
+        <div class="Frame_29_4">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame29_4_text1">5 Sites Licenses</p>
+        </div>
+        <div class="Frame_31_3">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame31_3_text1">Updates for 1 Year</p>
+        </div>
+        <div class="Frame_32_3">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame31_4_text1">Priority Email Support</p>
+        </div>
+        <div class="Frame_30_4">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame29_4_text1">2 Free Installation Service</p>
+        </div>
+        <div class="$299year">
+            <p><b class="price">$399</b> <b class="year">/Year</b></p>
+            <div class="Frame_12_2">
+             <p class="Frame12_2_button">Buy Now</p>
+
+            </div>
+            
+
+
+        
+    </div>
+       
+  
+  </div>
+
+
+ </div>
+
+
+ 
+    </div>
+    <div class="Frame_30_30">
+        <div class="Frame_33_33_33">
+            <div class="Frame_35_35">
+                <p class="Frame33_text1">25 Sites</p>
+                
+               </div>
+
+                  <div class="Frame_31_1">
+        <div class="Frame_29_4">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame29_4_text1">25 Sites Licenses</p>
+        </div>
+        <div class="Frame_31_3">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame31_3_text1">Updates for 1 Year</p>
+        </div>
+        <div class="Frame_32_3">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame31_4_text1">Priority Email Support</p>
+        </div>
+        <div class="Frame_30_4">
+            <i class="fa fa-check-circle" style="font-size:19px;color:rgb(249, 81, 81)"></i>
+           <p class="Frame29_4_text1">25 Free Installation Service</p>
+        </div>
+        <div class="$299year">
+            <p><b class="price">$599</b> <b class="year">/Year</b></p>
+            <div class="Frame_12_2">
+             <p class="Frame12_2_button">Buy Now</p>
+
+            </div>
+            
+
+
+        
+    </div>
+       
+  
+  </div>
+                
+    
+    
+            </div>
+        </div>
+       
+  
+  
+  </div>
+
+
+
+
+
+
+  
+
+ 
+</div>
+</div>
+ <!--slideshow-->
+ <div class="Frame_52">
+    <div class="Frame_35_35_35">
+        <p class="Frame_35_35_35_t">Resources for WooCommerce Store Owners</p>
+        <p class="Frame_35_35_35_t1">Tips, tricks and eCommerce inspiration from WooCommerce experts.</p>
+
+
+    </div>
+    <div class="swiper mySwiper">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <div class="image1_slide">
+               
+                <img class ="img1_slide" src="<?php echo get_template_directory_uri(); ?>/images/unsplash_cW4lLTavU80.png">
+                <div class="all_text">
+                    <div class="all_text1">
+                        <p class="img1_all_text1">Ecommerce</p>
+                    </div>
+                    <p class="img1_all_text2">Things to Keep in Mind While Pricing Your Product</p>
+                    <p class="img1_all_text3">Starting a business? Launching your dream product? You must... </p>
+
+                </div>
+               
+                
+
+            </div>
+           
+            
+
+            
+          </div>
+          <div class="swiper-slide">
+            <div class="image1_slide">
+               
+                <img class ="img1_slide" src="<?php echo get_template_directory_uri(); ?>/images/unsplash_h8CYq5InIAI.png">
+                <div class="all_text">
+                    <div class="all_text1">
+                        <p class="img1_all_text1">Ecommerce</p>
+                    </div>
+                    <p class="img1_all_text2">Why Your Website Speed Should Matter </p>
+                    <p class="img1_all_text3">Starting a business? Launching your dream product? You must... </p>
+
+                </div>
+               
+                
+
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="image1_slide">
+               
+                <img class ="img1_slide" src="<?php echo get_template_directory_uri(); ?>/images/unsplash_376KN_ISplE.png">
+                <div class="all_text">
+                    <div class="all_text1">
+                        <p class="img1_all_text1">Ecommerce</p>
+                    </div>
+                    <p class="img1_all_text2">Why you should have a booking extension on your website </p>
+                    <p class="img1_all_text3">Starting a business? Launching your dream product? You must... </p>
+
+                </div>
+               
+                
+
+            </div>
+          </div>
+          <div class="swiper-slide">
+            <div class="image1_slide">
+               
+                <img class ="img1_slide" src="<?php echo get_template_directory_uri(); ?>/images/unsplash_h8CYq5InIAI.png">
+                <div class="all_text">
+                    <div class="all_text1">
+                        <p class="img1_all_text1">Ecommerce</p>
+                    </div>
+                    <p class="img1_all_text2">Things to Keep in Mind While Pricing Your Product</p>
+                    <p class="img1_all_text3">Starting a business? Launching your dream product? You must... </p>
+
+                </div>
+               
+                
+
+            </div>
+          </div>
+          <div class="swiper-slide"><div class="image1_slide">
+               
+            <img class ="img1_slide" src="<?php echo get_template_directory_uri(); ?>/images/unsplash_cW4lLTavU80.png">
+            <div class="all_text">
+                <div class="all_text1">
+                    <p class="img1_all_text1">Ecommerce</p>
+                </div>
+                <p class="img1_all_text2">Things to Keep in Mind While Pricing Your Product</p>
+                <p class="img1_all_text3">Starting a business? Launching your dream product? You must... </p>
+
+            </div>
+           
+            
+
+        </div></div>
+          <div class="swiper-slide"><div class="image1_slide">
+               
+            <img class ="img1_slide" src="<?php echo get_template_directory_uri(); ?>/images/unsplash_376KN_ISplE.png">
+            <div class="all_text">
+                <div class="all_text1">
+                    <p class="img1_all_text1">Ecommerce</p>
+                </div>
+                <p class="img1_all_text2">Things to Keep in Mind While Pricing Your Product</p>
+                <p class="img1_all_text3">Starting a business? Launching your dream product? You must... </p>
+
+            </div>
+           
+            
+
+        </div>
+    </div>
+          
+        </div>
+      
+        <div class="swiper-pagination"></div>
+      </div>
+  
+      <!-- Swiper JS -->
+      <script src="<?php echo get_template_directory_uri();?>/https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.js"></script>
+  
+      <!-- Initialize Swiper -->
+      
+      <script>
+        var swiper = new Swiper(".mySwiper", {
+          slidesPerView: 3,
+          spaceBetween: 30,
+          slidesPerGroup: 3,
+          loop: true,
+          loopFillGroupWithBlank: true,
+          pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+          },
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+        });
+       
+      </script>
+      
+
+
+    
+      </div>
+<?php 
+   get_footer();
+
+?>
