8e52f0bee7e136005935e0a066dcbd7f87522bb1
Nadia Akther <nadia98akther@gmail.com>
2022-08-23 20:43:24 +0600

    template of contact page

---
+++ template-contact.php
@@ -0,0 +1,89 @@
+<?php 
+//Template Name: Contact
+
+get_header();
+?>
+
+
+
+<!--------Get in touch--------->
+<div class="Get_in_touch">
+    <div class="Frame_68">
+        <div class="Group2">
+            <diV class="text1_">
+                <p class="Group2_text1">Get in touch</p>
+
+            </diV>
+            <diV class="text2_">
+                <p class="Group2_text2">Fill the form and we will get back to you soon </p>
+
+            </diV>
+
+
+        </div>
+        <div class="Frame_67">
+           <div class="Frame_65">
+            <img class="icon1" src="<?php echo get_template_directory_uri(); ?>/images/bi_telephone.png" >
+             <p class="icontext">+8801795929299 </p>
+           </div>
+           <div class="Frame_66">
+            <img class="icon2" src="<?php echo get_template_directory_uri(); ?>/images/bi_envelope.png" >
+             <p class="icontext1">nadia98akther@gmail.com</p>
+           </div>
+           <div class="Frame_67_2">
+            <img class="icon3" src="<?php echo get_template_directory_uri(); ?>/images/Group (2).png" >
+             <p class="icontext2">48 Kadamtala, Basabo, Dhaka</p>
+           </div>
+        </div>
+        <div class="Frame_49_icon">
+            <img id="icon4" src="<?php echo get_template_directory_uri(); ?>/images/cib_facebook-f.png">
+            <img id="icon5" src="<?php echo get_template_directory_uri(); ?>/images/cib_linkedin-in.png">
+            <img id="icon6" src="<?php echo get_template_directory_uri(); ?>/images/cib_instagram.png">
+            <img id="icon7" src="<?php echo get_template_directory_uri(); ?>/images/cib_twitter.png">
+
+
+
+
+        </div>
+    </div>
+    <div class="Frame_31_Form">
+        <div class="Frame_69">
+            <p class="name">Your Name</p>
+            <div class="Frame_54">
+
+            </div>
+
+        </div>
+        <div class="Frame_70">
+            <p class="name">Email</p>
+            <div class="Frame_54">
+
+            </div>
+
+        </div>
+        <div class="Frame_71">
+            <p class="name">Subject</p>
+            <div class="Frame_54">
+
+            </div>
+
+        </div>
+        <div class="Frame_72">
+            <p class="name">Message</p>
+            <div class="Frame_5">
+
+            </div>
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+
+<?php 
+get_footer();
+
+?>
