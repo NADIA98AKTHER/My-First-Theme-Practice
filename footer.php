41a52762b242130485fa3931e5a09b3d7efb3d61
Nadia Akther <nadia98akther@gmail.com>
2022-08-23 20:47:21 +0600

    footer of my theme

---
+++ footer.php
@@ -0,0 +1,45 @@
+      <?php ?>
+      <!------------------Footer----------->
+      <div class="Frame_43">
+        <div class="Frame_48">
+            <div class="Frame_46">
+                <div class="Frame_44">
+                  <p class="Frame_44_t">Products</p>
+                  <p class="Frame_44_t2">WooCommerce Booking WooCommerce Subscription Advanced Coupons Checkout Page Customiser Bulk Pricing</p>
+                </div>
+                <div class="Frame_45">
+                    <p class="Frame_44_t">Resources</p>
+                    <p class="Frame_44_t2">Blog Documentation Support Forum Facebook Group Frequently Asked Questions</p>
+                </div>
+                <div class="Frame_46_2">
+                    <p class="Frame_44_t">Company</p>
+                    <p class="Frame_44_t2">Contact us Refund Policy Privacy Policy Affiliate Policy Terms and Conditions </p>
+                </div>
+                <div class="Frame_47">
+                    <p class="Frame_44_t">Partners</p>
+                    <p class="Frame_44_t2">Partnership Policy Join Our Affiliate Program!</p>
+                </div>
+
+            </div>
+            <div class="line">
+            </div>
+            <div id="Frame_50">
+                <div id="springdevs1">
+                    <img src="<?php echo get_template_directory_uri(); ?>/images/Springdevs (1) 1.png">
+                    <p class="copyright">Copyright © 2022 SpringDevs</p>
+                    <div class="Frame_49">
+                        <img class="f" src="<?php echo get_template_directory_uri(); ?>/images/cib_facebook-f.png">
+                        <img class="i" src="<?php echo get_template_directory_uri(); ?>/images/cib_instagram.png">
+                        <img class="l" src="<?php echo get_template_directory_uri(); ?>/images/cib_linkedin-in.png">
+                        <img class="t" src="<?php echo get_template_directory_uri(); ?>/images/cib_twitter.png">
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+
+      </div>
+</body>
+</html>
