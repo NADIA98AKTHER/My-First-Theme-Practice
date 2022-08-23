39c786aa55847ecc908ab725f4d6c87d4d7822d5
Nadia Akther <nadia98akther@gmail.com>
2022-08-23 20:46:34 +0600

    header file of my theme

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+   
+    <link rel="stylesheet" href=" <?php echo get_template_directory_uri(); ?>/https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>https://cdn.jsdelivr.net/npm/swiper/swiper-bundle.min.css"/>
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css">
+</head>
+<body>
+
+
+    <!--------Header----->
+
+
+  <!--------Menu----->
+  
+<div id="Frame_1">
+
+        
+    <?php wp_nav_menu (array('theme_location' =>'Primary-Menu','menu_class' =>'nav'))?>
+        
+        
+    
+</div>
+    <!--------Logo----->
+  
+    <div id="Springdevs">
+        <?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+$logo = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+ 
+if ( has_custom_logo() ) {
+    echo '<img src="' . esc_url( $logo[0] ) . '" alt="' . get_bloginfo( 'name' ) . '">';
+} else {
+    echo '<h1>' . get_bloginfo('name') . '</h1>';
+}?>
+       
+
+
+
+    </div>
+
+ 
+</div>
