ce3be866656ff33f2e72458af053c66a67a3b848
Nadia Akther <nadia98akther@gmail.com>
2022-08-23 20:48:16 +0600

    all functions of my theme

---
+++ functions.php
@@ -0,0 +1,27 @@
+<?php 
+register_nav_menus( 
+    array('Primary-Menu'=>'Top Menu')
+   
+) ;
+
+
+function themename_custom_logo_setup() {
+    $defaults = array(
+        'height'               => 100,
+        'width'                => 400,
+        'flex-height'          => true,
+        'flex-width'           => true,
+        'header-text'          => array( 'site-title', 'site-description' ),
+        'unlink-homepage-logo' => true, 
+    );
+ 
+    add_theme_support( 'custom-logo', $defaults );
+}
+ 
+add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
+
+//Featured images & Post thumbnails//
+	
+add_theme_support( 'post-thumbnails' );
+
+?>
