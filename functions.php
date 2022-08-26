2b8a9ef333747ec0da5523da36db61509fff2895
Nadia Akther <nadia98akther@gmail.com>
2022-08-26 20:59:41 +0600

    updated

--- functions.php
+++ functions.php
@@ -24,4 +24,34 @@ add_action( 'after_setup_theme', 'themename_custom_logo_setup' );
 	
 add_theme_support( 'post-thumbnails' );
 
+
 ?>
+<?php 
+function mytheme_custom_excerpt_length( $length ) {
+    return 10;
+}
+add_filter( 'excerpt_length', 'mytheme_custom_excerpt_length', 999 );
+
+
+?>
+<?php 
+add_filter('pre_get_posts', 'limit_archive_posts');
+function limit_archive_posts($query){
+    if ($query->is_archive) {
+        $query->set('posts_per_page', 2);
+    }
+    return $query;
+}
+?>
+<?php if ( have_posts() ) : ?>
+   
+   <!-- Start of the main loop. -->
+   <?php while ( have_posts() ) : the_post(); ?>
+     
+       <?php the_content(); ?>
+         
+    <?php wp_link_pages(); ?> 
+      
+   <?php endwhile; ?>
+ 
+<?php endif; ?>
