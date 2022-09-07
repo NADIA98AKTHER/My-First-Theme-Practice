43def4f905523759c85fcac79fdd2f60d7488779
Nadia <87221559+NADIA98AKTHER@users.noreply.github.com>
2022-09-07 18:48:38 +0600

    updated

--- 404.php
+++ 404.php
@@ -4,26 +4,9 @@ get_header();
 
 ?>
 
-<div id="primary" class="content-area">
-        <div id="content" class="site-content" role="main">
- 
-            <header class="page-header">
-                <h1 class="page-title"><?php _e( 'Not Found', 'twentythirteen' ); ?></h1>
-            </header>
- 
-            <div class="page-wrapper">
-                <div class="page-content">
-                    <h2><?php _e( 'This is somewhat embarrassing, isn’t it?', 'twentythirteen' ); ?></h2>
-                    <p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentythirteen' ); ?></p>
- 
-                    <?php get_search_form(); ?>
-                </div><!-- .page-content -->
-            </div><!-- .page-wrapper -->
- 
-        </div><!-- #content -->
-    </div><!-- #primary -->
-
-
+<div class="not_found">
+    <h1>Error 404</h1>
+</div>
 
 
 
