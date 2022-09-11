572e86c4189d0f03a89e0821a6857c9984866288
Nadia <87221559+NADIA98AKTHER@users.noreply.github.com>
2022-09-11 12:06:24 +0600

    updated

--- index.php
+++ index.php
@@ -90,83 +90,83 @@ wp_reset_postdata();
 
 <!--------Blog----------->
 <div class="Frame_63">
-    <div class="Frame_60">
+      <div class="Frame_60">
     
-       <div class="con2">
-      <div class="cat">
-         
+             <div class="con2">
+                     <div class="cat">
+                    
      
-      <div class="col-xs-7">
+                           <div class="col-xs-7">
      
-       <div class="Frame_59">
+                                <div class="Frame_59">
    
     
-        <div class="Frame_59_t1"><?php dynamic_sidebar('Add');?></div>
+                                          <div class="Frame_59_t1"><?php dynamic_sidebar('Add');?>
+                                           </div>
       
-        </div>
+                                </div>
        
      
-      </div>
+                            </div>
 
      
       
 
       
-</div>
+                      </div>
 
-</div>
+              </div>
 
-    </div>
+      </div>
 
     
   <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<div class="con">
+   <div class="con">
 
-  <div class="col-xs-6">
-  <?php 
-      while(have_posts()){
+         <div class="col-xs-6">
+              <?php 
+                while(have_posts()){
 
-      the_post();
-      $imagepath=wp_get_attachment_image_src(get_post_thumbnail_id(),'large');
+                 the_post();
+                   $imagepath=wp_get_attachment_image_src(get_post_thumbnail_id(),'large');
     
-    ?>
-    <div class="col">
+                 ?>
+                     <div class="col">
     
-      <div class="grid">  
-      <img class="prec" src="<?php echo $imagepath[0]?>">
-      <div class="grid_content">
+                         <div class="grid">  
+                              <img class="prec" src="<?php echo $imagepath[0]?>">
+                                   <div class="grid_content">
 
-      <div class="ecom">
-         <p class="ecom_t"><?php the_category(); ?></p>
+                                        <div class="ecom">
+                                                     <p class="ecom_t"><?php the_category(); ?></p>
          
-      </div>
-      <p class="ecom_t2"><?php the_title();?></p>
-         <div class="ecom_t3"><?php the_excerpt();?>
-         <div class="readmore">
-      <a href="<?php the_permalink();?>"><input type="button" value="Read More" name=""></a>
-      </div>
-        </div>
+                                          </div>
+                                               <p class="ecom_t2"><?php the_title();?></p>
+                                                     <div class="ecom_t3"><?php the_excerpt();?>
+                                                         <div class="readmore">
+                                                                 <a href="<?php the_permalink();?>"><input type="button" value="Read More" name=""></a>
+                                                           </div>
+                                                       </div>
        
 
-      </div>
+                                    </div>
       
+                          </div>
+  
+                      </div>
+                           
+                      <?php } ?>
       </div>
   
-    </div>
-    <?php } ?>
-    
-    <div>
-   
-   
-  </div>
-  
+                </div>
+ 
 </div>
 
-<div class="Page">
-    <div class="pagination">
-        <?Php next_posts_link(); ?>
-       <?php previous_posts_link(); ?>
-    </div>
+   
+         <div class="pagination">
+              <?Php next_posts_link(); ?>
+              <?php previous_posts_link(); ?>
+           </div>
       </div>
 <?php
 get_footer();
